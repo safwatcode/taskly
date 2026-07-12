edb701370d3d2adc35ca871e41c62ce95efbde7a
Safwat Ahmed <safwatcode@gmail.com>
2026-07-12 23:39:41 +0300

    feat: initialize Angular v21 project with pnpm, ESLint, and Prettier

---
+++ eslint.config.js
@@ -0,0 +1,47 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const eslintConfigPrettier = require('eslint-config-prettier');
+
+module.exports = tseslint.config(
+  // Global ESLint & TypeScript Rules for all typescript (.ts) files
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+    },
+  },
+
+  // Angular HTML Template Rules
+  {
+    files: ['**/*.html'],
+    extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
+    rules: {},
+  },
+
+  // Prettier Override (Must be the last item in the array)
+  eslintConfigPrettier,
+);
